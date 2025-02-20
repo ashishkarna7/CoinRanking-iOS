f634c5ee3a1f43518a8558f82858c7c723b2731f
Ashish Karna <a.karna@inspheris.com>
2025-02-20 20:00:02 +0545

    Fix: Initial project setup

---
+++ CoinRanking/CoinRankingTests/CoinRankingTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankingTests.swift
+//  CoinRankingTests
+//
+//  Created by Ashish Karna on 20/02/2025.
+//
+
+import Testing
+@testable import CoinRanking
+
+struct CoinRankingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
