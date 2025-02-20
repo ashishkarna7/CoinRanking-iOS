54439dd7e99e7a4c5b77faf308552311c73bd8aa
AshishKarna <ashish.karna7@gmail.com>
2025-02-20 23:31:53 +0545

    Fix: added missing file

---
+++ CoinRanking/CoinRanking/Module/Base/List/BaseListController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseListController.swift
+//  CoinRanking
+//
+//  Created by Ashish Karna on 20/02/2025.
+//
+
+import Foundation
+
+class BaseListController: BaseViewController {
+    
+    var screenView: BaseListView {
+        return baseView as! BaseListView
+    }
+    
+    var viewModel: BaseListViewModel {
+        return baseViewModel as! BaseListViewModel
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ CoinRanking/CoinRanking/Module/Base/Model/ContentLoadingType.swift
@@ -0,0 +1,14 @@
+//
+//  ContentLoadingType.swift
+//  CoinRanking
+//
+//  Created by Ashish Karna on 20/02/2025.
+//
+
+import Foundation
+
+enum ContentLoadingType {
+    case initial
+    case pagination
+    case refresh
+}
